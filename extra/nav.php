312e586ec4fd557bf335172d46dcb4eaf416944c
Zoe <zwzwatts@gmail.com>
2018-05-17 16:10:01 -0400

    nav css

--- extra/nav.php
+++ extra/nav.php
@@ -3,17 +3,17 @@
 <ul>
   <li><a href="home.php">Home</a></li>
   <li><a href="about.php">About Intellectus</a></li>
-  <li><a href="products.php">Products</a>
-    <ul>
+  <li class="hover"><a href="products.php">Products</a>
+    <ul class="hide">
       <li><a href="process-director.php">Process Director</a></li>
       <li><a href="rapid-workplace.php">RAPID Workplace</a></li>
       <li><a href="content-director.php">Content Director</a></li>
     </ul>
   </li>
-  <li><a href="services.php">Services</a>
-  <ul>
-    <li><a href="industry-solutions.php">Innovative Solutions</a></li>
-  </ul>
+  <li class="hover"><a href="services.php">Services</a>
+    <ul class="hide">
+      <li><a href="industry-solutions.php">Innovative Solutions</a></li>
+    </ul>
   </li>
   <li><a href="public-sector.php">Public Sector</a></li>
   <li><a href="partners.php">Partners</a></li>
