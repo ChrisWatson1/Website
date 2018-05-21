37dd2081e9bd2d43c145ada97c21f1f512eb0546
Zoe <zwzwatts@gmail.com>
2018-05-21 10:06:35 -0400

    Home

--- home.php
+++ home.php
@@ -10,17 +10,14 @@
 	 ?>
  <article class="main home">
 
-	<article class ='box'>
+<figure> <img src="images/featureIMG.jpg" alt="Government Contracts" /><figure>
 	<p> Committed to delivering innovation, Intellectus works with our clients helping them identify areas for reducing operational inefficiencies, drive process improvements, and achieve high performance.  </p>
- <figure> <img src="images/featureIMG.jpg" alt="Government Contracts" /><figure>
- </article>
+
 
 <article class ='box'>
 	<h3>Public Sector</h3>
-	<p>Intellectus saves time and money for our government customers providing a variety of electronic content management and business process automation solutions. </p>
-			<figure>
-						<img src="images/featureIMG2.jpg" alt="Government Contracts" />
-		 </figure>
+	<p>Intellectus saves time and money for our government customers providing a variety of electronic content management and business process automation solutions. Learn more about our <a href="public-sector.php"> Goverment Experience </a></p>
+
 	</article>
 
 <article class ='box'>
@@ -34,10 +31,6 @@
 <p> Committed to delivering innovation, Intellectus works with its clients to help them identify areas for reducing operational inefficiencies, drive process improvements, and achieve high performance. Working in partnership with our clients, Intellectus' professionals pair strategic acumen with strong execution to achieve results.</p>
 </article>
 
-<article class ='box'>
-			<h3>Public Sector</h3>
-			<p><a href="public-sector.php"><img src="images/fema1.jpg" ></a></p>
-</article>
 
 		</article>
 
