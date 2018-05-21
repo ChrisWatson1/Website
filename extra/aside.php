b6e0a06476bd0f45f4466552c424a0118293af66
Zoe <zwzwatts@gmail.com>
2018-05-21 11:10:42 -0400

    mobile

--- extra/aside.php
+++ extra/aside.php
@@ -2,22 +2,30 @@
 
 <h4>Contact Us</h4>
 <ul>
+	<div>
 	<li class="heading">All Locations</li>
 	<li>International: +1 703 439 1150</li>
 	<li>Fax: 440 425 1160</li>
 	<li>Email: Sales@Intellectus.com</li>
+  </div>
 
+  <div>
 	<li class="heading">Customer service:</li>
 	<li> Email: HelpDesk@Intellectus.com</td>
 	<li> Phone: 703 439 1160 </td>
+  </div>
 
+	<div>
 	<li class="heading">Northern Virginia</li>
 	<li>14220 Sullyfield Circle, Suite A</li>
 	<li>Chantilly, VA 20151</li>
+	</div>
 
+	<div>
 	<li class="heading">Charleston, South Carolina</li>
 	<li>300 Bucksley Lane, Suite 305</li>
 	<li>Charleston, SC 29492</li>
+</div>
 
 </ul>
 </aside>
