9d21e3c652b4095df6c12b9e8a8946cdea8b3188
Zoe <zwzwatts@gmail.com>
2018-05-21 10:21:53 -0400

    home

--- about.php
+++ about.php
@@ -5,8 +5,8 @@
 <figure> <img src="images/SecondaryHeaders16.jpg" alt="" /></figure>
 <h3> Intelligent Business Solutions </h3>
 
+<p> Committed to delivering innovation, Intellectus works with our clients helping them identify areas for reducing operational inefficiencies, drive process improvements, and achieve high performance.  </p>
 <p>Intellectus was founded as an <strong>On Demand, Business Solutions Company </strong>to help government agencies and commercial clients increase efficiency, reduce costs, and provide better services.</p>
 <p>We have extensive experience helping agencies and companies solve company critical business requirements by providing products and services that deliver real-time, targeted business solutions.</p>
 <p>To learn more about our services, contact us at 703.439.1150 or info@intellectus.com today to learn why organizations from 50 to over 500,000 people rely on us to power their global success</p>
-<p>Contact our sales team at 703 439 1140 today to learn why organizations from 50 to over 500,000 people rely on us to power their global success.</p>
 </article>
--- home.php
+++ home.php
@@ -10,19 +10,15 @@
 	 ?>
  <article class="main home">
 
-<figure> <img src="images/featureIMG.jpg" alt="Government Contracts" /><figure>
-	<p> Committed to delivering innovation, Intellectus works with our clients helping them identify areas for reducing operational inefficiencies, drive process improvements, and achieve high performance.  </p>
-
-
 <article class ='box'>
 	<h3>Public Sector</h3>
-	<p>Intellectus saves time and money for our government customers providing a variety of electronic content management and business process automation solutions. Learn more about our <a href="public-sector.php"> Goverment Experience </a></p>
+	<p>Intellectus saves time and money for our government customers providing a variety of electronic content management and business process automation solutions. Learn more about our <a href="public-sector.php"> Goverment Experience. </a></p>
 
 	</article>
 
 <article class ='box'>
 <h3>Your Business, Your Way </h3>
-<p>Intellectus delivers products and services solving essential business needs and delivering customer focused solutions.  We provide services ranging from cost effective business outsourcing to assisting clients with improving  internal business operations.<br>
+<p>Intellectus delivers products and services solving essential business needs and delivering customer focused solutions.  We provide services ranging from cost effective business outsourcing to assisting clients with improving  internal business operations.
 </p>
 </article>
 
