e0f92c29153f8b359bb5282dcede48a64992cf5c
Chris Watson <39312711+ChrisWatson1@users.noreply.github.com>
2018-05-23 08:53:13 -0400

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -1,1 +1,24 @@
 
+<aside id ='contact'>
+
+<h4>Contact Us</h4>
+<ul>
+	<li class="heading">All Locations</li>
+	<li>International: +1 703 439 1150</li>
+	<li>Fax: 440 425 1160</li>
+	<li>Email: Sales@Intellectus.com</li>
+
+	<li class="heading">Customer service:</li>
+	<li> Email: HelpDesk@Intellectus.com</td>
+	<li> Phone: 703 439 1160 </td>
+
+	<li class="heading">Northern Virginia</li>
+	<li>14220 Sullyfield Circle, Suite A</li>
+	<li>Chantilly, VA 20151</li>
+
+	<li class="heading">Charleston, South Carolina</li>
+	<li>300 Bucksley Lane, Suite 305</li>
+	<li>Charleston, SC 29492</li>
+
+</ul>
+</aside>
