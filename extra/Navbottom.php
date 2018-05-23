1a40de7966458c8ba90cde79873c7e3d64f71047
Chris Watson <39312711+ChrisWatson1@users.noreply.github.com>
2018-05-23 09:51:43 -0400

    Update Navbottom.php

--- extra/Navbottom.php
+++ extra/Navbottom.php
@@ -4,10 +4,11 @@
   <li><a href="home.php">Home</a></li>
   <li class="hover"><a href="products.php">Products and Services</a>
     <ul class="hide">
+      <li><a href="industry-solutions.php">Innovative Solutions</a></li>
       <li><a href="process-director.php">Process Director</a></li>
       <li><a href="rapid-workplace.php">RAPID Workplace</a></li>
       <li><a href="content-director.php">Content Director</a></li>
-      <li><a href="industry-solutions.php">Innovative Solutions</a></li>
+      
     </ul>
   </li>
   <li><a href="public-sector.php">Public Sector</a></li>
